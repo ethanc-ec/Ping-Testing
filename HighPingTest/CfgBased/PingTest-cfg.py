52b7b05bff2bf0bc353ca1ad590f6ac4c3b0c9ff
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2022-01-29 11:46:33 -0500

    Renamed variables

--- HighPingTest/CfgBased/PingTest-cfg.py
+++ HighPingTest/CfgBased/PingTest-cfg.py
@@ -1,93 +1,88 @@
 # pathlib for the file locations
-import pathlib as pl
+from pathlib import Path
 
-class CheckHighPing:
-    
-    def __init__(self, filename):
-        self.filename = filename
+def high_ping_checker(filename):
+    # Safely opens the file and closes it when done
+    with open(Path(__file__).parent / filename) as testLog:
+
+        # Copies the data from the file and puts each line into a list
+        contents = testLog.read().splitlines()
         
-    def HighPingChecker(filename):
-        # Safely opens the file and closes it when done
-        with open(pl.Path(__file__).parent / filename) as testLog:
-
-            # Copies the data from the file and puts each line into a list
-            contents = testLog.read().splitlines()
+        high_ping_times = {}
+        
+        for idx, reply in enumerate(contents):
             
-            HighPingTimes = {}
-            
-            for idx, reply in enumerate(contents):
+            # Skips past ping command, timeouts, etc.
+            if "Reply" in reply:
                 
-                # Skips past ping command, timeouts, etc.
-                if "Reply" in reply:
+                # Checks to see if ping has at least three digits by seeing if time={}ms has three ints
+                try:
                     
-                    # Checks to see if ping has at least three digits by seeing if time={}ms has three ints
-                    try:
-                        
-                        # Checks for ping w/ 3 or more digits
-                        int(reply[34:37])
-                        
-                        # Splits the selected string into separate pieces, able to take "time=___ms"
-                        pingTime = reply.split(" ")
-                        
-                        HighPingTimes[idx] = pingTime[4]
-                        
-                    # Skips that line if it does not start with "Reply"    
-                    except:
-                        pass
-                
-                # Just checks for timeouts 
-                elif "Request timed out." in reply:
-                    HighPingTimes[idx] = reply
-            
-            # Checks if there actually are high pings, if so, it'll print out the time and ping as a line
-            if HighPingTimes != {}:
-                print("Here is a list of the times with high ping:")
-                
-                for i in HighPingTimes:
-                    print(i, HighPingTimes[i])
+                    # Checks for ping w/ 3 or more digits
+                    int(reply[34:37])
                     
-            else:
-                return print("There were no instances of high ping")
+                    # Splits the selected string into separate pieces, able to take "time=___ms"
+                    ping_times = reply.split(" ")
+                    
+                    high_ping_times[idx] = ping_times[4]
+                    
+                # Skips that line if it does not start with "Reply"    
+                except:
+                    pass
+            
+            # Just checks for timeouts 
+            elif "Request timed out." in reply:
+                high_ping_times[idx] = reply
+        
+        # Checks if there actually are high pings, if so, it'll print out the time and ping as a line
+        if high_ping_times != {}:
+            print("Here is a list of the times with high ping:")
+            
+            for i in high_ping_times:
+                print(i, high_ping_times[i])
+                
+        else:
+            return print("There were no instances of high ping")
 
 
-def validFileInput():
+def valid_file_input():
     # Choose demo file or custom file
-    with open(pl.Path(__file__).parent / "PingTestConfig.cfg") as config:
+    with open(Path(__file__).parent / "PingTestConfig.cfg") as config:
         
-        configRead = config.read().splitlines()
+        config_read = config.read().splitlines()
         
-        if "True" in configRead[0]:
+        if "True" in config_read[0]:
             return "PingTestDemo.txt"
          
-        elif "False" in configRead[0]:
-            customeFileName = configRead[1].split("=")
-            return customeFileName[1]
+        elif "False" in config_read[0]:
+            custom_file_name = config_read[1].split("=")
+            return custom_file_name[1]
         
         else:
             return 'Failed'
             
             
-def fileCheckCaller(inputTestFile):
+def file_check_caller(input_test_file):
     # Actually checking the input file
     try:
-        return CheckHighPing.HighPingChecker(inputTestFile)
+        return high_ping_checker(input_test_file)
     
     # If it doesn't work it just prints an error message and goes to the exception function
     except:
         return print("A file with that name was not found in the same directory as the script")
 
                    
-def runFileFunctions():
+def run_file_functions():
     # Basically calls everything and actually runs the script
-    inputTestFile = validFileInput()
+    input_test_file = valid_file_input()
     
-    if "Failed" in inputTestFile:
+    if "Failed" in input_test_file:
         return print("The setting for UseDemoFile is incorrect, it needs to be True or False")
     
     else:
-        print("Checking file with name: {}".format(inputTestFile))
+        print("Checking file with name: {}".format(input_test_file))
     
-        return fileCheckCaller(inputTestFile)
+        return file_check_caller(input_test_file)
     
 if __name__ == "__main__":
-    runFileFunctions()
+    run_file_functions()
--- StabilityTest/StabTest-cfg.py
+++ StabilityTest/StabTest-cfg.py
@@ -1,43 +1,43 @@
-import pathlib as pl # pathlib for the file locations
+from pathlib import Path
 
-def HighPingChecker(filename):
+def high_ping_checker(filename):
     
-    configScaler = 1.00
+    config_scaler = 1.00
     
     #Reading config for the scaler
-    with open(pl.Path(__file__).parent / "StabTestConfig.cfg") as config:
+    with open(Path(__file__).parent / "StabTestConfig.cfg") as config:
         
-        configRead = config.read().splitlines()
+        config_read = config.read().splitlines()
         
-        configReadScale = configRead[2].split("=")
+        config_read_scaler = config_read[2].split("=")
         
-        configScaler = float(configReadScale[1])
+        config_scaler = float(config_read_scaler[1])
         
-        print("The ping comparison scaler is: {}".format(configScaler))
+        print("The ping comparison scaler is: {}".format(config_scaler))
         
     # Reading data file
-    with open(pl.Path(__file__).parent / filename) as testLog:
+    with open(Path(__file__).parent / filename) as test_log:
 
         # Copies the data from the file and puts each line into a list
-            contents = testLog.read().splitlines()
+            contents = test_log.read().splitlines()
             
-            HighPingTimes = {}
+            high_ping_times = {}
             
-            totalping, totalpingcount = 0, 0
+            total_pings, total_ping_count = 0, 0
             
             for i in contents:
                 if "Reply" in i:
-                    splitI = i.split(" ")
+                    split_i = i.split(" ")
                     
-                    pingTimeI = splitI[4]
+                    ping_time_i = split_i[4]
                 
-                    pingTime = int(pingTimeI[5:-2]) # Only has time="___"ms
+                    ping_time = int(ping_time_i[5:-2]) # Only has time="___"ms
                         
-                    totalping += pingTime
+                    total_pings += ping_time
                         
-                    totalpingcount += 1
+                    total_ping_count += 1
                 
-            avgPing = round(totalping/totalpingcount, 2)
+            avg_ping = round(total_pings/total_ping_count, 2)
             
             for idx, reply in enumerate(contents):
 
@@ -45,74 +45,74 @@ def HighPingChecker(filename):
                 if "Reply" in reply:
 
                     # Splits the selected string into separate pieces, able to take "time=___ms"
-                    contentSplit = reply.split(" ")
+                    content_split = reply.split(" ")
                     
-                    pingTimeMs = contentSplit[4] # Contains "time=___ms"
+                    ping_time_ms = content_split[4] # Contains "time=___ms"
                     
-                    pingTime = int(pingTimeMs[5:-2]) # Only has time="___"ms
+                    ping_time = int(ping_time_ms[5:-2]) # Only has time="___"ms
                     
-                    if pingTime > avgPing * configScaler:
-                        HighPingTimes[idx] = pingTimeMs
+                    if ping_time > avg_ping * config_scaler:
+                        high_ping_times[idx] = ping_time_ms
                 
                 # Just checks for timeouts 
                 elif "Request timed out." in reply:
-                    HighPingTimes[idx] = reply
+                    high_ping_times[idx] = reply
             
             # Checks if there actually are high pings, if so, it'll print out the time and ping as a line
-            if HighPingTimes != {}:
+            if high_ping_times != {}:
                 
-                print("The average ping was {} ms".format(avgPing))
+                print("The average ping was {} ms".format(avg_ping))
                 
                 print("Here is a list of the times where the ping was higher than the average:") 
                 
-                for i in HighPingTimes:
-                    print(i, HighPingTimes[i])
+                for i in high_ping_times:
+                    print(i, high_ping_times[i])
                     
             else:
                 print("There were no instances where the ping was higher than the average")
 
 
-def validFileInput():
+def valid_file_input():
     
     # Choose demo file or custom file
-    with open(pl.Path(__file__).parent / "StabTestConfig.cfg") as config:
+    with open(Path(__file__).parent / "StabTestConfig.cfg") as config:
         
-        configRead = config.read().splitlines()
+        config_read = config.read().splitlines()
         
-        if "True" in configRead[0]:
+        if "True" in config_read[0]:
             return "PingTestDemo.txt"
          
-        elif "False" in configRead[0]:
-            customFileName = configRead[1].split("=")
-            return customFileName[1]
+        elif "False" in config_read[0]:
+            custom_file_name = config_read[1].split("=")
+            return custom_file_name[1]
         
         else:
             return 'Failed'
             
             
-def fileCheckCaller(inputTestFile):
+def file_check_caller(input_test_file):
     
     # Actually checking the input file
     try:
-        return HighPingChecker(inputTestFile)
+        return high_ping_checker(input_test_file)
     
     # If it doesn't work it just prints an error message and goes to the exception function
     except:
         return print("Error: Either a file with the specified name does not exist or there is an issue with the config file")
 
                    
-def runFileFunctions():
+def run_file_functions():
     
     # Basically calls everything and actually runs the script
-    inputTestFile = validFileInput()
+    input_test_file = valid_file_input()
     
-    if "Failed" in inputTestFile:
+    if "Failed" in input_test_file:
         return print("Error: The setting for UseDemoFile is incorrect, it needs to be True or False")
     
     else:
-        print("Checking file with name: {}".format(inputTestFile))
+        print("Checking file with name: {}".format(input_test_file))
     
-        return fileCheckCaller(inputTestFile)
+        return file_check_caller(input_test_file)
     
 if __name__ == "__main__":
-    runFileFunctions()
+    run_file_functions()
